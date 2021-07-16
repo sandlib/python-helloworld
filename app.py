479b9883033964437a9082585e777c1be5cd5bf6
DESKTOP-D7NMFO9\Sandeep <www.sandeepbabu45gmail.com>
2021-07-16 12:36:34 +0530

    new added

--- app.py
+++ app.py
@@ -2,18 +2,17 @@ from flask import Flask
 from flask import json
 import logging
 
-
 app = Flask(__name__)
 
-@app.route("/status")
-def status():
+@app.route('/status')
+def healthcheck():
     response = app.response_class(
-        response = json.dumps({"result": "Ok-Healthy"}),
-        status = 200,
-        mimetype = 'application/json'
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
     )
-    #log
-    app.logger.info("status request successful")
+
+    app.logger.info('Status request successfull')
     return response
 
 @app.route('/metrics')
@@ -23,19 +22,18 @@ def metrics():
             status=200,
             mimetype='application/json'
     )
-    #log
-    app.logger.info('Metrics request successful')
+
+    app.logger.info('Metrics request successfull')
     return response
 
 @app.route("/")
 def hello():
-    #log
-    app.logger.info('main request successful')
+    app.logger.info('Main request successfull')
+
     return "Hello World!"
 
 if __name__ == "__main__":
-
-## stream logs to app.log file
-    logging.basicConfig(filename='app.log', level=logging.DEBUG)
-
+    ## stream logs to a file
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    
     app.run(host='0.0.0.0')
